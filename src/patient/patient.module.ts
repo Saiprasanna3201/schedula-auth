db4f8b659efd5f1bc7f24dd582cef03e253f4db5
Sai Prasanna <saiprasannagone2005@gmail.com>
2026-06-08 12:42:30 +0530

    Revert "feat: role-based auth with JWT for Doctor and Patient"
    This reverts commit 9a0a9c63a27722e90c75497bc6326e00656a60c3.

--- src/patient/patient.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PatientController } from './patient.controller';
-
-@Module({
-  controllers: [PatientController],
-})
-export class PatientModule {}
