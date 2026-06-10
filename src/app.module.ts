02f750d23c8e729caedb9fe2791bd1a7066ab84c
Sai Prasanna <saiprasannagone2005@gmail.com>
2026-06-10 12:31:11 +0530

    fix: restore app.module with AppModule export

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DoctorController } from './doctor.controller';
-import { DoctorProfileService } from './doctor-profile.service';
-import { DoctorProfile } from './doctor-profile.entity';
-import { DoctorDiscoveryController } from './doctor-discovery.controller';
-import { DoctorDiscoveryService } from './doctor-discovery.service';
+import { AuthModule } from './auth/auth.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { User } from './user.entity';
+import { DoctorProfile } from './doctor/doctor-profile.entity';
+import { PatientProfile } from './patient/patient-profile.entity';
+import { InitSchema1749000000000 } from './database/migrations/1749000000000-InitSchema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DoctorProfile])],
-  controllers: [DoctorController, DoctorDiscoveryController],
-  providers: [DoctorProfileService, DoctorDiscoveryService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [User, DoctorProfile, PatientProfile],
+      migrations: [InitSchema1749000000000],
+      synchronize: true,
+      migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    AuthModule,
+    DoctorModule,
+    PatientModule,
+  ],
 })
-export class DoctorModule {}
+export class AppModule {}
