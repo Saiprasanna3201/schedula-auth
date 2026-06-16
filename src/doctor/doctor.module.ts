77785a3394993a18d0550c1706a4da6e5bc92628
Sai Prasanna <saiprasannagone2005@gmail.com>
2026-06-16 17:22:31 +0530

    resolve merge conflict in app.module.ts

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -3,6 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorController } from './doctor.controller';
 import { DoctorProfileService } from './doctor-profile.service';
 import { DoctorProfile } from './doctor-profile.entity';
+<<<<<<< HEAD
+
+@Module({
+  imports: [TypeOrmModule.forFeature([DoctorProfile])],
+  controllers: [DoctorController],
+  providers: [DoctorProfileService],
+=======
 import { DoctorDiscoveryController } from './doctor-discovery.controller';
 import { DoctorDiscoveryService } from './doctor-discovery.service';
 import { DoctorAppointmentsController } from './doctor-appointments.controller';
@@ -15,5 +22,6 @@ import { AppointmentsModule } from '../appointments/appointments.module';
   ],
   controllers: [DoctorController, DoctorDiscoveryController, DoctorAppointmentsController],
   providers: [DoctorProfileService, DoctorDiscoveryService],
+>>>>>>> main
 })
 export class DoctorModule {}
