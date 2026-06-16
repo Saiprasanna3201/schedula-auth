e1b39a729398218dbbca6d3580525c5b8a977ce0
Sai Prasanna <saiprasannagone2005@gmail.com>
2026-06-16 17:42:11 +0530

    fix: restore app.module.ts content

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { SlotsModule } from './slots/slots.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { SchedulingModule } from './scheduling/scheduling.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        ssl: { rejectUnauthorized: false },
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+        synchronize: false,
+        migrationsRun: true,
+        logging: false,
+      }),
+    }),
+    AuthModule,
+    DoctorModule,
+    PatientModule,
+    SlotsModule,
+    AppointmentsModule,
+    SchedulingModule,
+  ],
+})
+export class AppModule {}
